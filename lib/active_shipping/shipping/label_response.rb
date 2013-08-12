fec125fb1fe3acacd0f8d3c4c195947bce9ae0c9
Jon Morton <jmorton@customink.com>
2013-08-12 09:48:34 -0400

    Remove dead code for parsing label response.

--- lib/active_shipping/shipping/label_response.rb
+++ lib/active_shipping/shipping/label_response.rb
@@ -23,25 +23,6 @@ module ActiveMerchant #:nodoc:
         end
       end
 
-      protected
-
-      # I'm following already established conventions.  Other response objects
-      # expect a hash like object.  This object is bound to vary from carrier
-      # to carrier, but I Ain't Gonna Need It...
-      def extract_package_data params
-        begin
-          packages = params["ShipmentResults"]["PackageResults"]
-          packages = [ packages ] if Hash === packages
-          @labels  = packages.map do |package|
-            { :tracking_number => package["TrackingNumber"],
-              :image => package["LabelImage"]
-            }
-          end
-        rescue RuntimeError => e
-          warn "Params do not appear to contain package data. #{params.keys}"
-        end
-      end
     end
-
   end
 end
