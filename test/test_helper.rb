6aee2bffbd2111d96381cc3be85c0adc6e478e97
Jon Morton <jmorton@customink.com>
2013-11-11 14:31:38 -0500

    Add missing fixture.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -97,6 +97,7 @@ module ActiveMerchant
         :big_half_pound => Package.new((16 * 50), [24,24,36], :units => :imperial),
         :chocolate_stuff => Package.new(80, [2,6,12], :units => :imperial),
         :declared_value => Package.new(80, [2,6,12], :units => :imperial, :currency => 'USD', :value => 999.99),
+        :tshirts => Package.new(10 * 16, nil, :units => :imperial),
         :shipping_container => Package.new(2200000, [2440, 2600, 6058], :description => '20 ft Standard Container', :units => :metric),
         :largest_gold_bar => Package.new(250000, [ 45.5, 22.5, 17 ], :value => 15300000)
       }
